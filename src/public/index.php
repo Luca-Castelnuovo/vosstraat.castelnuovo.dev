92a3a9baaf6026e1f88ce2bb1a826f4f4e83da04
Luca Castelnuovo <luca@castelnuovo.xyz>
2022-08-18 11:50:50 +0200

    add manifest

--- src/public/index.php
+++ src/public/index.php
@@ -18,6 +18,7 @@ if (isset($_GET['open'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./style.css" rel="stylesheet" />
+    <link href="/manifest.webmanifest" rel="manifest">
     <link rel="icon" type="image/png" href="/favicon.png" />
     <link rel="apple-touch-icon" href="/favicon.png">
     <title>Vosstraat Deur</title>
